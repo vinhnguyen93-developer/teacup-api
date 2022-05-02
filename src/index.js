c2d649aac03298428029bede2a685e01dc16d920
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-05-02 09:27:43 +0700

    [Update] Setup environment variable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const path = require('path');
-
 const app = express();
-const port = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
